4a9ccb4adcfb0e7ccccb429b5445da4eb9747902
slavka-ux <kib25-v.korobenko@nubip.edu.ua>
2026-07-12 22:05:10 +0300

    ci: add GitHub Actions workflow for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/flight-booking-service/',
   plugins: [react()],
   server: {
     port: 3000,
